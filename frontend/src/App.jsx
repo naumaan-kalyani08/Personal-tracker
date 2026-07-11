70b571b44beb6b0b5fc58ec8e3e49a0e3d57bed6
Naumaan <naumaan.kalyani02@gmail.com>
2026-07-11 22:30:27 +0530

    Added App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import RegistrationPage from './Pages/RegistrationPage'
+import './App.css'
+const App = () => {
+  return (
+    <div><RegistrationPage/></div>
+  )
+}
+
+export default App
