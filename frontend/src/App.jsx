76819a5d4f447d7c054edbcad652288cad8f5e33
Naumaan <naumaan.kalyani02@gmail.com>
2026-07-13 23:55:12 +0530

    testing Login feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import RegistrationPage from './Pages/RegistrationPage'
 import './App.css'
+import LoginPage from './Pages/LoginPage'
 const App = () => {
   return (
-    <div><RegistrationPage/></div>
+    <div>
+      {/* <RegistrationPage/> */}
+      <LoginPage/>
+    </div>
   )
 }
 
