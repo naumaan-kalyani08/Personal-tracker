a7844bacf4cced51f0df9cf811a876fef0706cee
Naumaan <naumaan.kalyani02@gmail.com>
2026-07-19 17:56:42 +0530

    testing routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import RegistrationPage from './Pages/RegistrationPage'
 import './App.css'
 import LoginPage from './Pages/LoginPage'
+import Header from './Components/UI/Header'
 const App = () => {
   return (
     <div>
       {/* <RegistrationPage/> */}
-      <LoginPage/>
+      {/* <LoginPage/> */}
+      <Header/>
     </div>
   )
 }
