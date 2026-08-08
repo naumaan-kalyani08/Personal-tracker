aa7d5fbd3696dd7485d28fe35ac34b085900a4be
Naumaan <naumaan.kalyani02@gmail.com>
2026-08-08 20:05:30 +0530

    Added Routing in Auth page

--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ReusableButton, ReusableInput } from '../Components/UI/ReusableComponents'
 import { useApiForm } from '../Api/ReusableApiLogics'
+import { useNavigate } from 'react-router'
 
 const InitialFormData = {
   email: '',
@@ -8,15 +9,24 @@ const InitialFormData = {
 }
 
 const LoginPage = () => {
+  const navigate = useNavigate()
   const { formData, handleInputChange, handleSubmit, loading, responseMessage } = useApiForm('login', InitialFormData)
-
+  const handleLoginSubmit = async (e) => {
+    const result = await handleSubmit(e)
+    
+    if (result && result.access_token) {
+      localStorage.setItem('access_token',result.access_token)
+      console.log('Login result:', result.access_token)
+      navigate('/dashboard')
+    }
+  }
   return (
     <div className="flex-box-center" style={{ height: '100vh' }}>
       <div className="registration-wrapper border p-4 rounded-lg shadow-md border-gray-300">
         <h2 className="text-xl font-bold mb-4">Login</h2>
         <ReusableInput label="Email" name="email" value={formData.email} onChange={handleInputChange} placeholder="" />
         <ReusableInput label="Password" name="password" type="password" value={formData.password} onChange={handleInputChange} placeholder="" />
-        <ReusableButton label="Login" onClick={handleSubmit} loading={loading} />
+        <ReusableButton label="Login" onClick={handleLoginSubmit} loading={loading} />
         {responseMessage && <p className="mt-3 text-sm text-red-500">{responseMessage}</p>}
       </div>
     </div>
