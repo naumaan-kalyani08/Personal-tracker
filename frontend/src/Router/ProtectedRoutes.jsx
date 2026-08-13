a045b0a20a8173bb2df53ec8c7e6976e359340f1
Naumaan <naumaan.kalyani02@gmail.com>
2026-08-13 20:59:44 +0530

    added protected routes

---
+++ frontend/src/Router/ProtectedRoutes.jsx
@@ -0,0 +1,16 @@
+﻿import React from 'react'
+import { Navigate, Outlet } from 'react-router'
+
+const isAuthenticated = () => {
+  return Boolean(localStorage.getItem('access_token'))
+}
+
+const ProtectedRoutes = () => {
+  if (!isAuthenticated()) {
+    return <Navigate to='/login' replace />
+  }
+
+  return <Outlet />
+}
+
+export default ProtectedRoutes
